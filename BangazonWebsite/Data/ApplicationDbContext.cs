774eff8c0df6ee79836be2f9969aa71f8f10ee63
Ryan McCarty <rmac_whf@yahoo.com>
2017-08-29 14:33:26 -0500

    temp

--- BangazonWebsite/Data/ApplicationDbContext.cs
+++ BangazonWebsite/Data/ApplicationDbContext.cs
@@ -22,5 +22,9 @@ namespace BangazonWebsite.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<BangazonWebsite.Models.ProductType> ProductType { get; set; }
+
+        public DbSet<BangazonWebsite.Models.Order> Order { get; set; }
     }
 }
