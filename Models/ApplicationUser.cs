8b7ff51f9dca43d4e8fb9d6d67c8601d8981bd03
Ryan McCarty <rmac_whf@yahoo.com>
2017-08-29 11:08:24 -0500

    first commit

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace BangazonWebsite.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string City { get; set; }
+        public string streetadress { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+
+    }
+}
